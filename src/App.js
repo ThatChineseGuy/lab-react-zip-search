61cf03f2f0fdd5c5e6860a83f02a7e4efad94ac6
ThatChineseGuy <104800276+ThatChineseGuy@users.noreply.github.com>
2022-09-23 16:01:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-function City(props) {
-  return <div>This is the City component</div>;
+function City(city, state, lat, long, EstimatedPopulation, TotalWages) {
+  return (<div>
+    <div class="answerhead">
+      {city},{state}
+    </div>
+      <ul>
+        <li>State: {state}</li>
+        <li>Location: ({lat},{long}</li>
+        <li>Population(estimated): {EstimatedPopulation}</li>
+        <li>Total Wages:{TotalWages}</li>
+      </ul>
+    
+    
+    </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return <div>This is the ZipSearchField component</div>;
+function ZipSearchField(value, change) {
+  return(<div>
+    <form method="get">
+      <label for="Zip Code">Zip Code:</label>
+      <input id="Zip Code" type="text" value={value}></input>
+    </form>
+  </div>
+  );
 }
 
 function App() {
+  const [data, setData] = useState("");
+  const [zipCode, setZipCode] = useState([]);
+
+  const zipchange = (event) => {
+    const newzip = event.target.value;
+    setData(newzip);
+  }
+
+  const fetchData = () => {
+    fetch('https://ctp-zip-api.herokuapp.com/zip/' + zipCode , {
+      'mode' : 'cors',
+      headers: {
+        'Content-Type' : 'application/json',
+      }
+    })
+    .then((response) => response.json())
+    .then((responseData) => setData(responseData))
+    .catch((error) => console.log(error));
+  }
   return (
     <div className="App">
       <div className="App-header">
         <h1>Zip Code Search</h1>
       </div>
       <div className="mx-auto" style={{ maxWidth: 400 }}>
-        <ZipSearchField />
+        <ZipSearchField value={data} change={zipchange}/>
         <div>
-          <City />
+          <City 
+            city ={setZipCode.City}
+            state = {setZipCode.state}
+            lat = {setZipCode.lat}
+            long={setZipCode.long}
+            EstimatedPopulation={setZipCode.EstimatedPopulation}
+            TotalWages={setZipCode.TotalWages}
+            />
           <City />
         </div>
       </div>
